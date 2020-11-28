63fddc9672c7d6787e086134248826eed9db04f9
ManfromBehind1 <ricky.535160005@stu.untar.ac.id>
2020-11-28 15:58:34 +0700

    menambahkan method get & set, constructor pada file school.java

--- src/com/ricky/School.java
+++ src/com/ricky/School.java
@@ -1,9 +1,105 @@
 package com.ricky;
 
+import java.util.List;
+
 public class School {
-
-	public School() {
-		// TODO Auto-generated constructor stub
+	private List<Teachers> teachers;
+	private List<Students> students;
+	private int totalMoneyEarned;
+	private int totalMoneySpent;
+	
+	/**
+	 * new school object is created.
+	 * @param teachers list of teachers in the school.
+	 * @param students list of students in the school.
+	 */
+	public School(List<Teachers> teachers, List<Students> students) {
+		super();
+		this.teachers = teachers;
+		this.students = students;
 	}
-
+	
+	/**
+	 * @return the list of the students.
+	 */
+	public List<Teachers> getTeachers() {
+		return teachers;
+	}
+	
+	/**
+	 * Add teacher to the school.
+	 * @param teachers the teacher to be added.
+	 */
+	public void addTeacher(Teachers teacher) {
+		teachers.add(teacher);
+	}
+	
+	public void setTeachers(List<Teachers> teachers) {
+		this.teachers = teachers;
+	}
+	
+	/**
+	 * 
+	 * @return the list of the student in the school.
+	 */
+	public List<Students> getStudents() {
+		return students;
+	}
+	
+	/**
+	 * Add the student to the school.
+	 * @param students the student to be added.
+	 */
+	/**
+	 * Add teacher to the school.
+	 * @param teachers the teacher to be added.
+	 */
+	public void addStudent(Students student) {
+		students.add(student);
+	}
+	
+	public void setStudents(List<Students> students) {
+		this.students = students;
+	}
+	
+	/**
+	 * 
+	 * @return the total money earned by the school.
+	 */
+	public int getTotalMoneyEarned() {
+		return totalMoneyEarned;
+	}
+	
+	/**
+	 * 
+	 * @param totalMoneyEarned 
+	 */
+	public void updateTotalMoneyEarned(int MoneyEarned) {
+		totalMoneyEarned += MoneyEarned;
+	}
+	
+	public void setTotalMoneyEarned(int totalMoneyEarned) {
+		this.totalMoneyEarned = totalMoneyEarned;
+	}
+	
+	/**
+	 * 
+	 * @return the total money spent by the school.
+	 */
+	public int getTotalMoneySpent() {
+		return totalMoneySpent;
+	}
+	
+	/**
+	 * 
+	 * @param totalMoneyEarned 
+	 */
+	public void updateTotalMoneySpent(int moneySpent) {
+		totalMoneyEarned += moneySpent;
+	}
+	
+	public void setTotalMoneySpent(int totalMoneySpent) {
+		this.totalMoneySpent = totalMoneySpent;
+	}
+	// Last vid min: 1:11:33
 }
