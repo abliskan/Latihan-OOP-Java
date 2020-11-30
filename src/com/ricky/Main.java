48f3adaa3d21dc91f3663413af997611577a53ea
ManfromBehind1 <ricky.535160005@stu.untar.ac.id>
2020-11-30 10:55:15 +0700

    Tambahin java list ke file main.java

--- src/com/ricky/Main.java
+++ src/com/ricky/Main.java
@@ -1,14 +1,35 @@
 package com.ricky;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
+//	public Main() {
+//		// TODO Auto-generated constructor stub
+//	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Teachers lizzy = new Teachers(1,"Lizzy",500);
+		Teachers mellisa = new Teachers(2,"Mellisa",700);
+		Teachers vanderhorn = new Teachers(3,"Vanderhorn",600);
+		
+		List<Teachers> teacherList = new ArrayList<>();
+		teacherList.add(lizzy);
+		teacherList.add(mellisa);
+		teacherList.add(vanderhorn);
+		
+		Students tamasha = new Students(1, "Tamasha",4);
+		Students rakshith = new Students(2,"Rakshith",12);
+		Students rabbi = new Students(3,"Rabbi",5);
+		List<Students> studentList = new ArrayList<>();
+		
+		studentList.add(tamasha);
+		studentList.add(rabbi);
+		studentList.add(rakshith);
+		
+		School ghs = new School(teacherList,studentList);
+		System.out.println("GHS has earned $" + ghs.getTotalMoneyEarned());
 	}
-
 }
